45cdb94ac05ccc669ba050dd793e6520b0752d2e
Olmo Jiménez <olmojial@gmail.com>
2015-01-07 17:07:39 +0100

    Improved ratings view.

--- public/controllers/main.js
+++ public/controllers/main.js
@@ -1,52 +1,9 @@
 angular.module('MyApp')
     .controller('MainCtrl', ['$scope', '$location', 'User', 'Rating', 'Week', function($scope, $location, User, Rating, Week) {
-        /*$scope.max = 5;
-        $scope.isReadonly = false;
-        $scope.rate = 2;
-
-        function getRatings() {
-            var rats = Rating.query();
-            $scope.vals = [];
-
-            $scope.users = User.query(function () {
-                var i, j;
-                var added = false;
-                for (i = 0; i < $scope.users.length; ++i) {
-                    added = false;
-                    for (j = 0; j < rats.length; ++j) {
-                        if (rats[j].player === $scope.users[i]._id) {
-                            $scope.vals.push({
-                                _id: rats[j]._id,
-                                user: rats[j].user,
-                                player: rats[j].player,
-                                value: rats[j].value
-                            });
-                            added = true;
-                            break;
-                        }
-                    }
-                    if (added === false) {
-                        $scope.vals.push({user: $scope.currentUser._id, player: $scope.users[i]._id, value: 0})
-                    }
-                }
-            });
-        }
-
-        getRatings();
-
-
-        $scope.setRating = function(id) {
-            Rating.save($scope.vals[id], function(){
-                getRatings();
-            });
-        };
-*/
 
         moment.locale('es');
 
         $scope.events = [];
-        var currentYear = moment().year();
-        var currentMonth = moment().month();
 
         $scope.weeks = Week.query(function(){
             var i;
@@ -56,40 +13,13 @@ angular.module('MyApp')
             }
         });
 
-        /*$scope.events = [
-            {
-                title: 'Event 1',
-                type: 'warning',
-                starts_at: new Date(currentYear,currentMonth,25,8,30),
-                ends_at: new Date(currentYear,currentMonth,25,9,30)
-            },
-            {
-                title: 'Event 2',
-                type: 'info',
-                starts_at: new Date(currentYear,currentMonth,19,7,30),
-                ends_at: new Date(currentYear,currentMonth,25,9,30)
-            },
-            {
-                title: 'This is a really long event title',
-                type: 'important',
-                starts_at: new Date(currentYear,currentMonth,25,6,30),
-                ends_at: new Date(currentYear,currentMonth,25,6,60)
-            },
-        ];*/
 
 
         $scope.calendarView = 'month';
         $scope.calendarDay = new Date();
 
         function showModal(action, event) {
-            /*$modal.open({
-                templateUrl: 'modalContent.html',
-                controller: function($scope, $modalInstance) {
-                    $scope.$modalInstance = $modalInstance;
-                    $scope.action = action;
-                    $scope.event = event;
-                }
-            });*/
+
         }
 
         $scope.eventClicked = function(event) {
--- public/controllers/valoraciones.js
+++ public/controllers/valoraciones.js
@@ -1,34 +1,38 @@
 angular.module('MyApp')
     .controller('ValoracionesCtrl', ['$scope', '$location', 'User', 'Rating', function($scope, $location, User, Rating) {
         $scope.max = 5;
-        $scope.isReadonly = false;
+        $scope.isReadonly = true;
         $scope.rate = 2;
 
         function getRatings() {
-            var ratings = Rating.query();
+
             $scope.vals = [];
 
             $scope.users = User.query(function () {
-                var i, j;
-                var added = false;
-                for (i = 0; i < $scope.users.length; ++i) {
-                    added = false;
-                    for (j = 0; j < ratings.length; ++j) {
-                        if (ratings[j].player === $scope.users[i]._id) {
-                            $scope.vals.push({
-                                _id: ratings[j]._id,
-                                user: ratings[j].user,
-                                player: ratings[j].player,
-                                value: ratings[j].value
-                            });
-                            added = true;
-                            break;
+                var ratings = Rating.query(function () {
+                    var i, j;
+                    var added = false;
+                    for (i = 0; i < $scope.users.length; ++i) {
+                        added = false;
+                        for (j = 0; j < ratings.length; ++j) {
+                            if (ratings[j].player === $scope.users[i]._id) {
+                                $scope.vals.push({
+                                    _id: ratings[j]._id,
+                                    user: ratings[j].user,
+                                    player: ratings[j].player,
+                                    value: ratings[j].value
+                                });
+                                added = true;
+                                break;
+                            }
+                        }
+                        if (added === false) {
+                            $scope.vals.push({user: $scope.currentUser._id, player: $scope.users[i]._id, value: 0})
                         }
                     }
-                    if (added === false) {
-                        $scope.vals.push({user: $scope.currentUser._id, player: $scope.users[i]._id, value: 0})
-                    }
-                }
+
+                    $scope.isReadonly = false;
+                });
             });
         }
 
