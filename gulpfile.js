ffab1251953a1e8518a5e6db31ad74d6b428d6d1
Olmo Jiménez <olmojial@gmail.com>
2015-01-21 16:42:43 +0100

    css optimized in one file with gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var templateCache = require('gulp-angular-templatecache');
 var uncss = require('gulp-uncss');
+var glob = require('glob');
 
 gulp.task('sass', function() {
     gulp.src('public/stylesheets/style.scss')
@@ -17,11 +18,37 @@ gulp.task('sass', function() {
                 'public/views/home.html',
                 'public/views/login.html',
                 'public/views/signup.html',
-                'public/views/addWeek.html'
+                'public/views/addWeek.html',
+                'public/views/viewWeek.html',
+                'public/views/valoraciones.html',
+                'public/views/adminUsers.html'
+            ]
+        }))*/
+        //.pipe(csso())
+        .pipe(gulp.dest('public/stylesheets'));
+
+    gulp.src([
+        'public/stylesheets/*.css',
+        'public/stylesheets/style.css'
+        ])
+        .pipe(concat('style.css'))
+        /*.pipe(uncss({
+            html: [
+                'public/index.html',
+                'public/views/home.html',
+                'public/views/login.html',
+                'public/views/signup.html',
+                'public/views/addWeek.html',
+                'public/views/viewWeek.html',
+                'public/views/valoraciones.html',
+                'public/views/adminUsers.html',
+                'public/vendor/angular-bootstrap-calendar-tpls.js',
+                'public/vendor/ui-bootstrap-custom-tpls-0.12.0.js',
+                'public/vendor/select.js'
             ]
         }))*/
         .pipe(csso())
-        .pipe(gulp.dest('public/stylesheets'));
+        .pipe(gulp.dest('public'));
 });
 
 gulp.task('watch', function() {
@@ -53,4 +80,4 @@ gulp.task('templates', function() {
         .pipe(gulp.dest('public'));
 });
 
-gulp.task('default', ['sass', 'compress', 'templates', 'watch']);
+gulp.task('default', ['templates', 'sass', 'compress', 'watch']);
